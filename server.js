fbb4454cd3ae46db05dc24350f3de78774a4fb9a
Mrbossna <bossza2468@gmail.com>
2024-06-01 00:09:08 +0700

    update

--- server.js
+++ server.js
@@ -5,36 +5,36 @@ const dotenv = require('dotenv');
 dotenv.config();
 // เปิดใช้ CORS
 fastify.register(fastifyCors, {
-    origin: '*'
+  origin: '*'
 });
 
 // เชื่อมต่อกับ MongoDB
 fastify.register(fastifyMongo, {
-    url: `${process.env.MONGODB}`
+  url: process.env.MONGODB
 });
 
 // ตัวอย่าง route
 fastify.get('/data', async (request, reply) => {
-    const collection = fastify.mongo.db.collection('list');
-    const result = await collection.find().toArray();
-    reply.send(result);
+  const collection = fastify.mongo.db.collection('list');
+  const result = await collection.find().toArray();
+  reply.send(result);
 });
 
 fastify.post('/data', async (request, reply) => {
-    const collection = fastify.mongo.db.collection('list');
-    const result = await collection.insertOne(request.body);
-    reply.send(result);
-})
+  const collection = fastify.mongo.db.collection('list');
+  const result = await collection.insertOne(request.body);
+  reply.send(result);
+});
 
 // เริ่มเซิร์ฟเวอร์
 const start = async () => {
-    try {
-        await fastify.listen({ port: 3000 });
-        fastify.log.info(`Server is running on http://localhost:3000`);
-    } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
+  try {
+    await fastify.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' });
+    fastify.log.info(`Server is running on http://localhost:${process.env.PORT || 3000}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 };
 
 start();
